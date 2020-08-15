85d890d090efbdef2cdb07851c1d371859cea1c7
Luis Chitala <53163045+luischitala@users.noreply.github.com>
2020-08-14 23:14:36 -0500

    first project with R

---
+++ calculosr.R
@@ -0,0 +1,36 @@
+4 + 8
+
+20 - 8
+
+4^2
+
+x <- 86
+x
+
+Oficina <- 7
+Platzi <- 1
+Transporte <- 1.5
+
+Tiempo_al_dia <- Oficina + Platzi + Transporte
+Tiempo_al_dia
+
+Corte_1 <- 0.3
+Corte_2 <- 0.3
+Corte_3 <- 0.4
+
+Nota_1 <- 4.0
+Nota_2 <- 4.6
+Nota_3 <- 3.0
+
+Nota_c_1 <- Nota_1 * Corte_1
+Nota_c_1
+
+Nota_c_2 <- Nota_2 * Corte_2
+Nota_c_2
+
+Nota_c_3 <- Nota_3 * Corte_3
+Nota_c_3
+
+Nota_final <- Nota_c_1 + Nota_c_2 + Nota_c_3
+Nota_final
+
